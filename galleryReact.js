e56be5b358bda0aab04d784270eeba4edb1677ff
AnthonyJordan <Kain235@gmail.com>
2023-12-12 20:05:16 -0500

    started NavBar

--- galleryReact.js
+++ galleryReact.js
@@ -1,6 +1,6 @@
-// import React, { useState, useEffect } from "react";
-// import Footer from "./Footer";
-// import descriptionConfig from "./description-config.json" assert { type: "json" };
+//import Footer from "./Footer.js";
+import descriptionConfig from "./description-config.json" assert { type: "json" };
+import NavBar from "./NavBar.js";
 ("use strict");
 
 const e = React.createElement;
@@ -73,230 +73,31 @@ class GalleryReact extends React.Component {
   // });
 
   render() {
-    return React.createElement("div", null, "hi");
+    return React.createElement(
+      React.Fragment,
+      null,
+      React.createElement(
+        "div",
+        { id: "hotherym3" },
+        React.createElement(
+          "a",
+          { href: "main.html" },
+          React.createElement("img", {
+            src: "images/hotherym-small.png",
+            height: "59",
+            width: "273",
+          }),
+          React.createElement(NavBar, null)
+        ),
+        "hi"
+      )
+    );
     // <>
     //   {console.log(test)}
     //   <div id="hotherym3">
     //     <a href="main.html">
     //       <img src="images/hotherym-small.png" height="59" width="273" />
     //     </a>
-    //   </div>
-    //   <div id="navbar" class="dropdown">
-    //     <p class="dropbtn">The Navigator</p>{" "}
-    //     <div class="dropdown-content">
-    //       <div class="home-container">
-    //         <a
-    //           href="art.html"
-    //           id="Art"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link"
-    //         >
-    //           Art
-    //         </a>
-    //         <a
-    //           href="scraps.html"
-    //           id="Scraps"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link01"
-    //         >
-    //           Scraps
-    //         </a>
-    //         <a
-    //           href="showcase.html"
-    //           id="Showcase"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link02"
-    //         >
-    //           Showcase
-    //         </a>
-    //         <a
-    //           href="kid.html"
-    //           id="Kid"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link03"
-    //         >
-    //           Kid
-    //         </a>
-    //         <a
-    //           href="traditional.html"
-    //           id="Traditional"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link04"
-    //         >
-    //           Traditional
-    //         </a>
-    //         <a
-    //           href="digital.html"
-    //           id="Digital"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link05"
-    //         >
-    //           Digital
-    //         </a>
-    //         <a
-    //           href="collaboration.html"
-    //           id="Collaboration"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link06"
-    //         >
-    //           Collaboration
-    //         </a>
-    //         <a
-    //           href="photography.html"
-    //           id="Photography"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link07"
-    //         >
-    //           Photography
-    //         </a>
-    //         <a
-    //           href="animalia.html"
-    //           id="Animalia"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link08"
-    //         >
-    //           Animalia
-    //         </a>
-    //         <a
-    //           href="anthropoda.html"
-    //           id="Anthropoda"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link09"
-    //         >
-    //           Anthropoda
-    //         </a>
-    //         <a
-    //           href="reptilia.html"
-    //           id="Reptilia"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link10"
-    //         >
-    //           Reptilia
-    //         </a>
-    //         <a
-    //           href="aves.html"
-    //           id="Aves"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link11"
-    //         >
-    //           Aves
-    //         </a>
-    //         <a
-    //           href="mammalia.html"
-    //           id="Mammalia"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link12"
-    //         >
-    //           Mammalia
-    //         </a>
-    //         <a
-    //           href="miscellarthropoda.html"
-    //           id="Miscellantropoda"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link13"
-    //         >
-    //           Miscellarthropoda
-    //         </a>
-    //         <a
-    //           href="arachnida.html"
-    //           id="Arachnida"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link14"
-    //         >
-    //           Arachnida
-    //         </a>
-    //         <a
-    //           href="insecta.html"
-    //           id="Insecta"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link15"
-    //         >
-    //           Insecta
-    //         </a>
-    //         <a
-    //           href="miscellanimalia.html"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link16"
-    //         >
-    //           Miscellanimalia
-    //         </a>
-    //         <a
-    //           href="inanimalia.html"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link17"
-    //         >
-    //           Inanimalia
-    //         </a>
-    //         <a
-    //           href="architecture.html"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link18"
-    //         >
-    //           Architecture
-    //         </a>
-    //         <a
-    //           href="vista.html"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link19"
-    //         >
-    //           Vista
-    //         </a>
-    //         <a
-    //           href="objecta.html"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link20"
-    //         >
-    //           Objecta
-    //         </a>
-    //         <a
-    //           href="sculptures.html"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link21"
-    //         >
-    //           Sculptures
-    //         </a>
-    //         <a
-    //           href="digital.html"
-    //           id="Digital"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link22"
-    //         >
-    //           Digital
-    //         </a>
-    //         <a
-    //           href="analogue.html"
-    //           id="Analogue"
-    //           target="_parent"
-    //           rel="noreferrer"
-    //           class="home-link23"
-    //         >
-    //           Analogue
-    //         </a>
-    //       </div>
-    //     </div>
     //   </div>
     //   <div id="galleryContainer">
     //     <a></a>
