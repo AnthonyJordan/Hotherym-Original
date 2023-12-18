b8e863410f388d90940f58516a9555daf2af352d
AnthonyJordan <Kain235@gmail.com>
2023-12-18 16:21:43 -0500

    small React progress

--- galleryReact.js
+++ galleryReact.js
@@ -5,64 +5,56 @@ import NavBar from "./NavBar.js";
 
 const e = React.createElement;
 
-class GalleryReact extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  // folders = [
-  //   "albums/arts/collaboration/",
-  //   "albums/arts/scraps/digital/",
-  //   "albums/arts/scraps/kid/",
-  //   "albums/arts/scraps/traditional/",
-  //   "albums/arts/showcase/analogue/",
-  //   "albums/arts/showcase/digital/",
-  //   "albums/photography/animalia/arthropoda/arachnida/",
-  //   "albums/photography/animalia/arthropoda/insecta/",
-  //   "albums/photography/animalia/arthropoda/miscellarthropoda/",
-  //   "albums/photography/animalia/aves/",
-  //   "albums/photography/animalia/mammalia/",
-  //   "albums/photography/animalia/miscellanimalia/",
-  //   "albums/photography/animalia/reptilia/",
-  //   "albums/photography/inanimalia/architecture/",
-  //   "albums/photography/inanimalia/objecta/",
-  //   "albums/photography/inanimalia/vista/",
-  //   "albums/sculpt/",
-  // ];
+function GalleryReact() {
+  const folders = [
+    "albums/arts/collaboration/", // 0
+    "albums/arts/scraps/digital/", // 1
+    "albums/arts/scraps/kid/", // 2
+    "albums/arts/scraps/traditional/", // 3
+    "albums/arts/showcase/analogue/", // 4
+    "albums/arts/showcase/digital/", // 5
+    "albums/photography/animalia/arthropoda/arachnida/", // 6
+    "albums/photography/animalia/arthropoda/insecta/", // 7
+    "albums/photography/animalia/arthropoda/miscellarthropoda/", // 8
+    "albums/photography/animalia/aves/", // 9
+    "albums/photography/animalia/mammalia/", //10
+    "albums/photography/animalia/miscellanimalia/", //11
+    "albums/photography/animalia/reptilia/", // 12
+    "albums/photography/inanimalia/architecture/", // 13
+    "albums/photography/inanimalia/objecta/", // 14
+    "albums/photography/inanimalia/vista/", // 15
+    "albums/sculpt/", // 16
+  ];
 
-  //   folders.forEach((folder) => {
-  //     var imageName = "";
+  const images = {};
 
-  //     $.ajax({
-  //       url: folder,
-  //       success: function (data) {
-  //         $(data)
-  //           .find("a")
-  //           .attr("href", function (i, val) {
-  //             if (val.match(/\.(jpe?g|png|gif)$/)) {
-  //               (imageName = val.split("/")),
-  //                 (imageName = imageName.pop().split("."));
-  //               (imageName = imageName[0]),
-  //                 $(document.getElementById("galleryContainer")).append(
-  //                   "<div id=galleryImageDiv><img src='" +
-  //                     val +
-  //                     "'  id=galleryImage></div>"
-  //                 );
-  //             }
-  //           });
-  //       },
-  //       error: function (error) {
-  //         console.log(error);
-  //       },
-  //     });
-  //   });
+  folders.forEach((folder) => {
+    var imageName = "";
+    images[folder] = new Array();
 
-  // componentDidMount() {
-  //   fetch("albums/arts/collaboration/").then((r) => {
-  //     if (r.ok) {
-  //       console.log(r);
-  //     }
-  //   });
-  // }
+    $.ajax({
+      url: folder,
+      success: function (data) {
+        $(data)
+          .find("a")
+          .attr("href", function (i, val) {
+            if (val.match(/\.(jpe?g|png|gif)$/)) {
+              (imageName = val.split("/")),
+                (imageName = imageName.pop().split("."));
+              (imageName = imageName[0]),
+                images[folder].push(
+                  "<div id=galleryImageDiv><img src='" +
+                    val +
+                    "'  id=galleryImage></div>"
+                );
+            }
+          });
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    });
+  });
 
   //Make mouse scroll left and right
   //item = document.getElementById("galleryContainer");
@@ -72,40 +64,38 @@ class GalleryReact extends React.Component {
   //   else item.scrollLeft -= 200;
   // });
 
-  render() {
-    return React.createElement(
-      React.Fragment,
-      null,
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(
+      "div",
+      { id: "hotherym3" },
       React.createElement(
-        "div",
-        { id: "hotherym3" },
-        React.createElement(
-          "a",
-          { href: "main.html" },
-          React.createElement("img", {
-            src: "images/hotherym-small.png",
-            height: "59",
-            width: "273",
-          }),
-          React.createElement(NavBar, null)
-        ),
-        "hi"
-      )
-    );
-    // <>
-    //   {console.log(test)}
-    //   <div id="hotherym3">
-    //     <a href="main.html">
-    //       <img src="images/hotherym-small.png" height="59" width="273" />
-    //     </a>
-    //   </div>
-    //   <div id="galleryContainer">
-    //     <a></a>
-    //   </div>
+        "a",
+        { href: "main.html" },
+        React.createElement("img", {
+          src: "images/hotherym-small.png",
+          height: "59",
+          width: "273",
+        }),
+        React.createElement(NavBar, null)
+      ),
+      "hi"
+    )
+  );
+  // <>
+  //   {console.log(test)}
+  //   <div id="hotherym3">
+  //     <a href="main.html">
+  //       <img src="images/hotherym-small.png" height="59" width="273" />
+  //     </a>
+  //   </div>
+  //   <div id="galleryContainer">
+  //     <a></a>
+  //   </div>
 
-    //   <Footer />
-    // </>
-  }
+  //   <Footer />
+  // </>
 }
 const domContainer = document.querySelector("#galleryReact");
 const root = ReactDOM.createRoot(domContainer);
