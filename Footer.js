6e7096d323700dbaf7505709737e68eff99de0e7
AnthonyJordan <Kain235@gmail.com>
2023-12-20 17:25:56 -0500

    description file built

--- Footer.js
+++ Footer.js
@@ -4,10 +4,6 @@ function Footer() {
     { id: "footer" },
     "Hotherym.net � 2007-2023 to Eorhythm. Web design (mostly) by Neolmas"
   );
-  // <div id="footer">
-  //   &nbsp;&nbsp;&nbsp; Hotherym.net � 2007-2023 to Eorhythm. Web design
-  //   (mostly) by Neolmas
-  // </div>
 }
 
 export default Footer;
